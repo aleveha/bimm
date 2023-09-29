9f3f103d2a4b91ef9d48c67e333dff975ca21258
Nikita Tashchilin <nikita.tashilin@gmail.com>
2023-09-29 15:16:38 -0400

    feat(backend): add database service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from "./database/database.module";
 import { MakesModule } from "./makes/makes.module";
 import { UtilsModule } from "./utils/utils.module";
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), MakesModule, UtilsModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, MakesModule, UtilsModule],
 	controllers: [],
 	providers: [],
 })
